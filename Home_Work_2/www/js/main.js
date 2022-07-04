4dee792804af0fbdd956972d0be43e58fe1d7b6d
Slysar Nikolay <slysarnikolay@Slysars-MacBook-Pro.local>
2022-07-04 23:07:29 +0300

    first commit

---
+++ Home_Work_2/www/js/main.js
@@ -0,0 +1,28 @@
+let userName = prompt('What is your name?').trim();
+let lastName = prompt('What is your last name?').trim();
+let email = prompt('Write your Email please!').trim().toLowerCase();
+let firstMailSymbol, 
+    lastMailSymbol, 
+    mailData;
+let mailSymbol = email.indexOf('@');
+let yearOfBirth = +prompt('What is your year of birth?').trim().replaceAll(" ","");
+let currentYear = new Date().getFullYear();
+let age = currentYear - yearOfBirth;
+
+if (mailSymbol === -1) {
+    mailData = "Email: Not valid Email: " + email + " (symbol @ is not exist)";
+} else if  (firstMailSymbol = email.startsWith('@')){
+    mailData = "Email: Not valid email " + email + " (symbol @ find in first place)";
+} else if (lastMailSymbol = email.endsWith('@')){
+    mailData = "Not valid Email: " + email + " (symbol @ find in last place)";
+} else{
+    mailData = "Email: " + email;
+}
+
+document.write(`
+    <ul class= "form">
+        <li>${"Full name: " + userName + " " + lastName}</li>
+        <li>${mailData}</li>
+        <li>${"Age: " + age}</li>
+    </ul>
+`);
