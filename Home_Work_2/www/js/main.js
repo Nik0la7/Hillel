4824d17b65452eb063fecb07e3571ac96690f1e1
Slysar Nikolay <slysarnikolay@Slysars-MacBook-Pro.local>
2022-07-05 11:33:40 +0300

    fix errors

--- Home_Work_2/www/js/main.js
+++ Home_Work_2/www/js/main.js
@@ -1,6 +1,6 @@
 let userName = prompt('What is your name?').trim();
 let lastName = prompt('What is your last name?').trim();
-let email = prompt('Write your Email please!').trim().toLowerCase();
+let email = prompt('Write your Email please!').trim().toLowerCase().replaceAll(" ","");;
 let firstMailSymbol, 
     lastMailSymbol, 
     mailData;
@@ -11,9 +11,9 @@ let age = currentYear - yearOfBirth;
 
 if (mailSymbol === -1) {
     mailData = "Email: Not valid Email: " + email + " (symbol @ is not exist)";
-} else if  (firstMailSymbol = email.startsWith('@')){
+} else if  (email.startsWith('@')){
     mailData = "Email: Not valid email " + email + " (symbol @ find in first place)";
-} else if (lastMailSymbol = email.endsWith('@')){
+} else if (email.endsWith('@')){
     mailData = "Not valid Email: " + email + " (symbol @ find in last place)";
 } else{
     mailData = "Email: " + email;
