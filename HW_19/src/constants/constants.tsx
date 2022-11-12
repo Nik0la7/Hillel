ab732616dca15297fa6cd1a5c93fdd35f87ebb10
Slysar Nikolay <slysarnikolay@Slysars-MacBook-Pro.local>
2022-11-12 22:01:38 +0200

    app

--- HW_19/src/constants/constants.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export const API_URL = 'https://api.tvmaze.com/shows'
-export const PEOPLE_URL = 'https://api.tvmaze.com/people'
-export const SEARCH_URL = 'https://api.tvmaze.com/singlesearch/shows?%20q='
-export const regex = /<[^<>]+>/g;
-
-
-
